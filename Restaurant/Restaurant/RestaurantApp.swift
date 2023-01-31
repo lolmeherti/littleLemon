5ee802c8943f67ecf254866e8c710778e5108e7d
Alex H <deampuleadd@yandex.com>
2023-01-30 21:38:39 -0800

    [INITIAL] commit

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Alex H. on 30.01.23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
