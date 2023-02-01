056ad3ed12a291d0e501b1aec20783796a5accab
Alex H <deampuleadd@yandex.com>
2023-02-01 23:41:37 +0100

    [ADDS] enums for navigation routing

---
+++ Restaurant/Restaurant/Enums/NavigationEnums.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationEnums.swift
+//  Restaurant
+//
+//  Created by Alex H. on 31.01.23.
+//
+
+import Foundation
+
+enum Navigation:String,Hashable
+{
+    case onboarding = "Onboarding"
+    case home = "Home"
+    case menu = "Menu"
+    case profile = "Profile"
+}
