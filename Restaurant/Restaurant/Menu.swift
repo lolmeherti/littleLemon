2ea3526daabcd2c0fd858fcd8f8ed752469af280
Alex H <deampuleadd@yandex.com>
2023-02-01 23:40:32 +0100

    [ADDS] menu view shell

---
+++ Restaurant/Restaurant/Menu.swift
@@ -0,0 +1,32 @@
+//
+//  Menu.swift
+//  Restaurant
+//
+//  Created by Alex H. on 31.01.23.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        
+        VStack
+        {
+            Text("Little Lemon")
+            Text("Chicago")
+            Text("Welcome to the little lemon restaurant app. Order food with ease!")
+            
+            List
+            {
+                
+            }
+        }
+       
+    }
+}
+
+struct Menu_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu()
+    }
+}
