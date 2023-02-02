3cbb4836a70d6b2bb878ffaf1fc6b466b9b50747
Alex H <deampuleadd@yandex.com>
2023-02-02 05:53:55 +0100

    [CHANGES] hides navigation button back

--- Restaurant/Restaurant/Home.swift
+++ Restaurant/Restaurant/Home.swift
@@ -6,18 +6,23 @@
 //
 
 import SwiftUI
-
 struct Home: View {
+    
     var body: some View {
-       
+        
         TabView
         {
             Menu()
                 .tabItem{
-                    Label("Home", systemImage: "list.dash")
-                    
+                    Label("Menu", systemImage: "list.dash")
+                }
+                
+            UserProfile()
+                .tabItem{
+                    Label("Profile", systemImage: "square.and.pencil")
                 }
         }
+        .navigationBarBackButtonHidden(true)
         
     }
 }
