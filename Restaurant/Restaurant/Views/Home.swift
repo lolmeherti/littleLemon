253f182b9470bbd1fe6929d105d39826e66c7559
Alex H <deampuleadd@yandex.com>
2023-02-04 08:38:15 +0100

    [ADDS] persistence controller to environment

--- Restaurant/Restaurant/Views/Home.swift
+++ Restaurant/Restaurant/Views/Home.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 struct Home: View {
     
+    let persistence = PersistenceController.shared
+    
     var body: some View {
         
         TabView
         {
             Menu()
+                .environment(\.managedObjectContext, persistence.container.viewContext)
                 .tabItem{
                     Label("Menu", systemImage: "list.dash")
                 }
@@ -22,6 +25,7 @@ struct Home: View {
                     Label("Profile", systemImage: "square.and.pencil")
                 }
         }
+        
         .navigationBarBackButtonHidden(true)
         
     }
